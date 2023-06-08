e7d614d4c2f1f3ce62943383abca30da3237a78c
Igor de Paulo Maia <igor.maia@syngenta.com>
2023-06-08 18:52:20 -0500

    comentários entidades

--- src/app/entities/CitacaoEntity.ts
+++ src/app/entities/CitacaoEntity.ts
@@ -3,18 +3,23 @@ import ColecaoEntity from './ColecaoEntity';
 
 @Entity('citacao')
 class CitacaoEntity {
+    // Identificador único gerado automaticamente
     @PrimaryGeneratedColumn('increment')
     id?: number;
 
+    // Título da citação
     @Column({ length: 1000, type: 'varchar', nullable: false })
     titulo: string;
 
+    // Data de criação da citação
     @CreateDateColumn({ name: 'created_at' })
     created_at?: Date;
 
+    // ID da coleção associada à citação
     @Column({ name: 'id_colecao', type: 'int', nullable: false })
     id_colecao: number;
 
+    // Relacionamento de um-para-muitos com a entidade de coleção
     @OneToMany(() => ColecaoEntity, (colecao) => colecao.citacoes)
     colecao?: ColecaoEntity;
 }
--- src/app/entities/ColecaoEntity.ts
+++ src/app/entities/ColecaoEntity.ts
@@ -3,26 +3,33 @@ import CitacaoEntity from './CitacaoEntity';
 
 @Entity('colecao')
 class ColecaoEntity {
+    // Identificador único gerado automaticamente
     @PrimaryGeneratedColumn('increment')
     id: number;
 
+    // Título da coleção
     @Column({ length: 100, type: 'varchar', nullable: false })
     titulo: string;
 
+    // Subtítulo da coleção (opcional)
     @Column({ length: 100, type: 'varchar', nullable: true })
     subtitulo?: string;
 
+    // URL ou caminho da imagem representando a coleção
     @Column({ type: 'varchar' })
     imagem: string;
 
+    // Autor da coleção
     @Column({ length: 100, type: 'varchar', nullable: false })
     autor: string;
 
+    // Data de criação da coleção
     @CreateDateColumn({ name: 'created_at' })
     created_at?: Date;
 
+    // Relacionamento de um-para-muitos com a entidade de citação
     @OneToMany(() => CitacaoEntity, (citacao) => citacao.colecao)
-    citacoes?: CitacaoEntity[]
+    citacoes?: CitacaoEntity[];
 }
 
 export default ColecaoEntity;
