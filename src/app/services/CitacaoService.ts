1c8024f9746e896dcaaf4b0058a44ec9fd138eac
Igor de Paulo Maia <igor.maia@syngenta.com>
2023-06-08 18:48:01 -0500

    comentários citacao service

--- src/app/services/CitacaoService.ts
+++ src/app/services/CitacaoService.ts
@@ -14,14 +14,17 @@ class CitacaoService implements CitacaoServiceInterface {
     citacaoRepository: CitacaoRepositoryInterface,
     colecaoRepository: ColecaoRepositoryInterface
   ) {
+    // Injeta as dependências dos repositórios de citação e coleção
     this.citacaoRepository = citacaoRepository;
     this.colecaoRepository = colecaoRepository;
   }
 
+  // Retorna todas as citações ou as citações filtradas por título
   async findAll(titulo?: string): Promise<CitacaoEntity[]> {
     return await this.citacaoRepository.findAll(titulo);
   }
 
+  // Retorna uma citação pelo ID fornecido, caso exista. Caso contrário, lança um erro de não encontrado
   async findById(id: number): Promise<CitacaoEntity> {
     const citacao = await this.citacaoRepository.findById(id);
     if (citacao) {
@@ -33,6 +36,7 @@ class CitacaoService implements CitacaoServiceInterface {
     });
   }
 
+  // Cria uma nova citação com o título e ID da coleção fornecidos
   async create({
     titulo,
     id_colecao,
@@ -40,6 +44,7 @@ class CitacaoService implements CitacaoServiceInterface {
     return await this.citacaoRepository.create({ titulo, id_colecao });
   }
 
+  // Atualiza uma citação pelo ID fornecido, com os campos de título e/ou ID da coleção (caso fornecidos)
   async update(
     id: number,
     { titulo, id_colecao }: CitacaoInterface
@@ -71,6 +76,7 @@ class CitacaoService implements CitacaoServiceInterface {
     return await this.citacaoRepository.update(id, citacao);
   }
 
+  // Exclui uma citação pelo ID fornecido, caso exista. Caso contrário, lança um erro de não encontrado
   async delete(id: number): Promise<void> {
     const citacao = await this.citacaoRepository.getCitacaoOnly(id);
     if (!citacao) {
