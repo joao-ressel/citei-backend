8fa5faf38dd1a3395d719963f5e602b9bc352006
Igor de Paulo Maia <igor.maia@syngenta.com>
2023-06-08 18:53:53 -0500

    comentários database source

--- src/app/validators/CitacaoValidator.ts
+++ src/app/validators/CitacaoValidator.ts
@@ -1,15 +1,23 @@
 import { body } from 'express-validator';
 
+// Validador para a criação de uma nova citação
 export const CreateCitacaoValidator = [
+  // Verifica se o campo 'titulo' não está vazio e é uma string
   body('titulo').notEmpty().isString(),
+  // Verifica se o campo 'id_colecao' não está vazio e é um número inteiro
   body('id_colecao').notEmpty().isInt(),
 ];
 
+// Validador para a atualização de uma citação existente
 export const UpdateCitacaoValidator = [
+  // Verifica se o campo 'titulo' é opcional, e caso esteja presente, é uma string com tamanho entre 3 e 255 caracteres
   body('titulo').optional().isString().isLength({ min: 3, max: 255 }),
+  // Verifica se o campo 'id_colecao' é opcional, e caso esteja presente, é um número inteiro
   body('id_colecao').optional().isNumeric().isInt(),
 ];
 
+// Validador para a busca de citações
 export const GetCitacaoValidator = [
+  // Verifica se o campo 'titulo' é opcional, e caso esteja presente, é uma string com tamanho máximo de 255 caracteres
   body('titulo').optional().isString().isLength({ max: 255 }),
-]
+];
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,16 +5,17 @@ import { CreateCitacaoTable1685674751168 } from './migrations/1685674751168-Crea
 import ColecaoEntity from "../app/entities/ColecaoEntity"
 import CitacaoEntity from "../app/entities/CitacaoEntity"
 
+// Cria uma nova instância de DataSource para configurar a conexão com o banco de dados
 export const AppDataSource = new DataSource({
-    type: process.env.DB_TYPE as any,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT as any,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [ColecaoEntity, CitacaoEntity],
-    migrations: [CreateColecaoTable1685670316914, CreateCitacaoTable1685674751168],
-    subscribers: [],
+    type: process.env.DB_TYPE as any,     // Tipo de banco de dados definido no arquivo .env
+    host: process.env.DB_HOST,            // Host do banco de dados definido no arquivo .env
+    port: process.env.DB_PORT as any,     // Porta do banco de dados definida no arquivo .env
+    username: process.env.DB_USER,        // Nome de usuário do banco de dados definido no arquivo .env
+    password: process.env.DB_PASS,        // Senha do banco de dados definida no arquivo .env
+    database: process.env.DB_NAME,        // Nome do banco de dados definido no arquivo .env
+    synchronize: true,                    // Sincroniza as entidades com o esquema do banco de dados
+    logging: false,                       // Desativa o log do TypeORM
+    entities: [ColecaoEntity, CitacaoEntity],  // Entidades usadas pela aplicação
+    migrations: [CreateColecaoTable1685670316914, CreateCitacaoTable1685674751168],  // Migrações usadas pela aplicação
+    subscribers: [],                      // Subscribers usados pela aplicação
 })
